d4b786526671f2c53fab2d35c17e65d4983068c1
xw <wei.xiong@marketingforce.com>
2023-08-29 17:17:30 +0800

    feat: lint&prettier配置

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,30 +1,30 @@
+<template>
+    <div class="base-header">
+        <div class="left">Element Plus</div>
+        <div class="right">
+            <div class="theme-config" h="full" @click="toggleDark()">
+                <i inline-flex i="dark:ep-moon ep-sunny" />
+            </div>
+        </div>
+    </div>
+</template>
+
 <script lang="ts" setup>
 import { toggleDark } from "~/composables";
 </script>
 
-<template>
-  <div class="base-header">
-    <div class="left">Element Plus</div>
-    <div class="right">
-      <div class="theme-config" h="full" @click="toggleDark()">
-        <i inline-flex i="dark:ep-moon ep-sunny" />
-      </div>
-    </div>
-  </div>
-</template>
-
 <style lang="scss" scoped>
 .base-header {
-  height: 60px;
-  border-bottom: solid 1px #dcdfe6;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 14px;
-  padding: 0 20px;
-  .theme-config {
+    height: 60px;
+    border-bottom: solid 1px #dcdfe6;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
     padding: 0 20px;
-    cursor: pointer;
-  }
+    .theme-config {
+        padding: 0 20px;
+        cursor: pointer;
+    }
 }
 </style>
